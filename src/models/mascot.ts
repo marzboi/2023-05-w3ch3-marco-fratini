83b8251fd710b76381398fbdd6235572f4641150
Marco Fratini <marcofratini@outlook.com>
2023-05-17 17:39:02 +0200

    Add animal list

--- src/models/mascot.ts
+++ src/models/mascot.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-export type mascotStructure = {
+export type petStructure = {
   chip: string;
   name: string;
   race: string;
@@ -7,15 +7,17 @@ export type mascotStructure = {
   owner: string;
 };
 
-export class Mascot implements mascotStructure {
+export class Pet implements petStructure {
   static generateChip() {
     return Math.trunc(Math.random() * 1_000_000).toString();
   }
 
   chip: string;
   isAdopted: boolean;
-  constructor(public name: string, public race: string, public owner: string) {
-    this.chip = Mascot.generateChip();
+  owner: string;
+  constructor(public name: string, public race: string) {
+    this.chip = Pet.generateChip();
     this.isAdopted = false;
+    this.owner = '';
   }
 }
