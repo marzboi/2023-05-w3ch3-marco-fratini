7191d6ee619345cedca5dcdd06a808103b184e68
Marco Fratini <marcofratini@outlook.com>
2023-05-17 17:51:36 +0200

    Add header component

---
+++ src/components/header.ts
@@ -0,0 +1,17 @@
+import { Component } from './component';
+
+export class Header extends Component {
+  constructor(selector: string) {
+    super(selector);
+    this.template = this.createTemplate();
+    this.render();
+  }
+
+  createTemplate() {
+    return `
+    <header>
+      <h1>Marco's Amazing Pets</h1>
+    </header>
+    `;
+  }
+}
